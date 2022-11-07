7a40de4033732c10e074612a6ab66ab6c93eb9ac
am <am@acorello.it>
2022-11-07 19:05:48 +0100

    Test extraction of ISBN from PDF

--- arkivist.py
+++ arkivist.py
@@ -1,7 +1,11 @@
-import itertools
+import itertools as itt
 from pathlib import *
 from dataclasses import dataclass
-import typing as ty
+from typing import *
+import PyPDF2 as pdf
+import isbnlib
+from isbnlib import RE_ISBN13
+import pipe
 
 
 @dataclass(
@@ -31,16 +35,31 @@ def path_by_publisher(b: BookInfo) -> PurePath:
     )
 
 
-def books(folder: Path) -> ty.Iterable[Path]:
+def books(folder: Path) -> Iterable[Path]:
     "Traverse folder looking book files"
     pdfs = folder.rglob("*.pdf")
     epubs = folder.rglob("*.epub")
-    return itertools.chain(pdfs, epubs)
+    return itt.chain(pdfs, epubs)
 
 
-def resolve_metadata(file: Path) -> BookInfo:
+def _isbn_of_pdf(file: Path):
+    pdfr = pdf.PdfReader(file)
+    map = pipe.map
+    isbns = (
+        pdfr.pages[:5]
+        | map(pdf.PageObject.extract_text)
+        | map(RE_ISBN13.finditer)
+        | pipe.traverse
+        | map(Match.group)
+        | map(isbnlib.canonical)
+    )
+    return next(isbns, None)
+
+
+def book_info(file: Path) -> BookInfo | None:
     "Extract ISBN from file; resolve Title, Year, Author, Publisher"
-    ...
+    if isbn := _isbn_of_pdf(file):
+        return BookInfo(isbn=isbn, title=None, publisher=None, year=0, ext=None)
 
 
 # Candidate Book: a PDF, an ePub, …
@@ -50,7 +69,7 @@ BASE_FOLDER = Path(Path.home(), "Documents", "Books, Magazines, Articles")
 def organize(base_path: Path) -> None:
     for book_file in books(base_path):
         try:
-            bookinfo = resolve_metadata(book_file)
+            bookinfo = book_info(book_file)
             new_path = base_path.joinpath(path_by_isbn(bookinfo))
             print(f"Moving {book_file} to {new_path}")
         except Exception as e:
---
+++ arkivist_test.py
@@ -0,0 +1,55 @@
+import arkivist as arkv
+import pathlib as pl
+import datetime as dt
+import shutil as sh
+import pytest as pyt
+
+"""markdown
+# Goal 1
+
+Given a folder with electronics books stored as `*.pdf` or `*.epub` files in various places
+
+I want to organize them in the following paths:
+
+- `$BASE/By-ISBN/{ISBN} • {Title} • {Year} • {Publisher}/{Title}.{ext}`
+- `$BASE/By-Publisher/{Title} • {Year}/{Title}.{ext}`
+
+## Challenge
+
+1. Reliably extract book’s ISBN: the proxy for all other attributes.
+    - Strategy 1: parse ePub or PDF in folder and search for the ISBN.
+2. Fetch book’s attributes from online sources (is isbntools sufficient?)
+"""
+
+
+@pyt.fixture
+def books_folder(pytestconfig: pyt.Config) -> pl.Path:
+    sample_books = pytestconfig.rootpath.joinpath("sample_books")
+    samples_copy = pl.Path().joinpath(
+        "tmp", f"sample_books_{dt.datetime.now().isoformat()}"
+    )
+    sh.copytree(sample_books, samples_copy)
+    yield samples_copy
+    sh.rmtree(samples_copy)
+
+
+def books_test(books_folder: pl.Path):
+    assert books_folder.exists()
+    assert sum(1 for _ in arkv.books(books_folder)) == 4
+
+
+def resolve_metadata_test(pytestconfig: pyt.Config):
+    sample_book = pytestconfig.rootpath.joinpath(
+        "sample_books",
+        "By-ISBN",
+        "978-1-098-10483-2 ÷ Knowledge Graphs - Data in Context for Responsive Businesses ÷ O'Reilly",
+        "book.pdf",
+    )
+    assert arkv._isbn_of_pdf(sample_book) == "9781098104832"
+    # arkv.BookInfo(
+    #     isbn="9781098104832",
+    #     title="Knowledge Graphs - Data in Context for Responsive Businesses",
+    #     year=2020,
+    #     publisher="O'Reilly",
+    #     ext="pdf",
+    # )
